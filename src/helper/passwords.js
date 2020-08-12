433799b9e4d7149bd47763fe506acc8ee2bd11bd
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2020-08-12 22:58:00 +0500

    Added on apartment asdfjasdf

--- src/helper/passwords.js
+++ src/helper/passwords.js
@@ -1,26 +1,26 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-exports.encryptPassword = async password => {
-  let promisedData = await bcrypt
-    .hash(password, saltRounds)
-    .then(hash => {
-      return hash;
-    })
-    .catch(error => {
-      return false;
-    });
-  return promisedData;
+exports.encryptPassword = async (password) => {
+	let promisedData = await bcrypt
+		.hash(password, saltRounds)
+		.then((hash) => {
+			return hash;
+		})
+		.catch((error) => {
+			return false;
+		});
+	return promisedData;
 };
 
 exports.decryptPassword = async (password, hash) => {
-  let passwordMatch = await bcrypt
-    .compare(password, hash)
-    .then(result => {
-      return result;
-    })
-    .catch(error => {
-      return false;
-    });
-  return passwordMatch;
+	let passwordMatch = await bcrypt
+		.compare(password, hash)
+		.then((result) => {
+			return result;
+		})
+		.catch((error) => {
+			return false;
+		});
+	return passwordMatch;
 };
